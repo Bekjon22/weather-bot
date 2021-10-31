2d3fd419dac08845f672bb8b14e0d54519fc4049
BekjonBakhromov <bakhromovbekjon2211@gmail.com>
2021-10-31 19:49:50 +0500

    second commit( bot link on main2 class )

--- src/main/java/uz/pdp/Main2.java
+++ src/main/java/uz/pdp/Main2.java
@@ -22,28 +22,8 @@ public class Main2 {
             e.printStackTrace();
         }
 
+//todo bot link-->   @DailyWeatherUzbekistanBot
 
-
-//        URL url = new URL("api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=be4c5331a79136ca700df931fb0b98d5");
-
-
-//        String city ="Tashkent";
-//
-//        try {
-//
-//            Gson gson = new Gson();
-//            URL url = new URL("http://api.openweathermap.org/data/2.5/weather?q=Sirdaryo&appid=be4c5331a79136ca700df931fb0b98d5");
-//            URLConnection connection = url.openConnection();
-//            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-//            Response response = gson.fromJson(reader, Response.class);
-//            for (WeatherItem weatherItem : response.getWeather()) {
-//                System.out.println(weatherItem.getMain());
-//            }
-//
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
 
 
     }
